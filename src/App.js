46b54a0b01e6eed898450598de705d6af6258590
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2023-09-15 19:02:20 +0000

    undo

--- src/App.js
+++ src/App.js
@@ -20,32 +20,31 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [load, upadateLoad] = useState(true);
 
-  // useEffect(() => {
-  //   const timer = setTimeout(() => {
-  //     upadateLoad(false);
-  //   }, 1200);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      upadateLoad(false);
+    }, 1200);
 
-  //   return () => clearTimeout(timer);
-  // }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
 
-    // <Router basename="/my-portfolio">
-    //   <Preloader load={load} />
-    //   <div className="App" id={load ? "no-scroll" : "scroll"}>
-    //     <Navbar />
-    //     <ScrollToTop />
-    //     <Routes>
-    //       <Route path="/" element={<Home />} />
-    //       <Route path="/project" element={<Projects />} />
-    //       <Route path="/about" element={<About />} />
-    //       <Route path="/resume" element={<Resume />} />
-    //       <Route path="*" element={<Navigate to="/"/>} />
-    //     </Routes>
-    //     <Footer />
-    //   </div>
-    // </Router>
-    <h1>Hello World</h1>
+    <Router basename="/my-portfolio">
+      <Preloader load={load} />
+      <div className="App" id={load ? "no-scroll" : "scroll"}>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="*" element={<Navigate to="/"/>} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
