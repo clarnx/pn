7e88aed1f91a8f7c54a90087c0b7e6f987fbda1f
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2023-09-15 19:18:40 +0000

    debug 3

--- src/App.js
+++ src/App.js
@@ -30,13 +30,27 @@ function App() {
 
   return (
 
-    <Router basename="/my-portfolio">
-      <Preloader load={load} />
+    // <Router basename="/my-portfolio">
+    //   <Preloader load={load} />
+    //   <div className="App" id={load ? "no-scroll" : "scroll"}>
+    //     <Navbar />
+    //     <ScrollToTop />
+    //     <Routes>
+    //       <Route path="/" element={<Home />} />
+    //       <Route path="/project" element={<Projects />} />
+    //       <Route path="/about" element={<About />} />
+    //       <Route path="/resume" element={<Resume />} />
+    //       <Route path="*" element={<Navigate to="/"/>} />
+    //     </Routes>
+    //     <Footer />
+    //   </div>
+    // </Router>
+
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
@@ -44,7 +58,7 @@ function App() {
         </Routes>
         <Footer />
       </div>
-    </Router>
+    
   );
 }
 
