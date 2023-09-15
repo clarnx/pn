0b129d0abcf187119ce2515ba66838130feaec69
A.E Clarence <33183127+clarnx@users.noreply.github.com>
2023-09-15 18:53:48 +0000

    debugging adding hello world

--- src/App.js
+++ src/App.js
@@ -20,13 +20,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [load, upadateLoad] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      upadateLoad(false);
-    }, 1200);
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //     upadateLoad(false);
+  //   }, 1200);
 
-    return () => clearTimeout(timer);
-  }, []);
+  //   return () => clearTimeout(timer);
+  // }, []);
 
   return (
 
@@ -34,14 +34,15 @@ function App() {
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
-        <ScrollToTop />
+        {/* <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/"/>} />
-        </Routes>
+        </Routes> */}
+        <h1>Hello World</h1>
         <Footer />
       </div>
     </Router>
